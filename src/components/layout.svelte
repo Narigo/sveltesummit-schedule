7fc8bfb9c5ea7504471e40bab11ce9bd2ea3a68a
Joern Bernhardt <joern.bernhardt@compose.us>
2021-05-06 01:06:21 +0200

    Center main content
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/components/layout.svelte
+++ src/components/layout.svelte
@@ -3,7 +3,9 @@
 		<slot name="header">hello</slot>
 	</header>
 </slot>
-<slot />
+<main>
+	<slot />
+</main>
 <slot name="full-footer"
 	><footer>
 		<slot name="footer">hello footer</slot>
@@ -16,6 +18,11 @@
 		background-color: #666;
 		display: flex;
 		margin: 0;
-		padding: 0;
+		padding: 2em;
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 </style>
