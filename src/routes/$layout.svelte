3811ef953e58f136aa351509ceb4c243140ec31b
Joern Bernhardt <joern.bernhardt@compose.us>
2021-05-07 19:58:00 +0200

    Use sans-serif font for now
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -2,6 +2,8 @@
 
 <style>
 	:global(html, body) {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
 		margin: 0;
 		padding: 0;
 	}
