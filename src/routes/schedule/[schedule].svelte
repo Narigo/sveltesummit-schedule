d75e5bf210e3d73b1bd13d2ae563db62a921f594
Joern Bernhardt <joern.bernhardt@compose.us>
2021-08-23 00:44:24 +0200

    Add UTC offsets
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/routes/schedule/[schedule].svelte
+++ src/routes/schedule/[schedule].svelte
@@ -5,20 +5,20 @@
 	let schedule: string = $page.params.schedule;
 
 	const timezones = [
-		{ value: -8 * 60, label: 'San Francisco (PDT)' },
-		{ value: -7 * 60, label: 'Salt Lake City (MDT)' },
-		{ value: -6 * 60, label: 'Chicago (CDT), Mexico City (CDT)' },
-		{ value: -5 * 60, label: 'New York City (EDT), Santiago (CLT)' },
-		{ value: -4 * 60, label: 'Buenos Aires (ART), São Paulo (BRT)' },
-		{ value: -1 * 60, label: 'Accra (GMT), Reykjavik (GMT)' },
-		{ value: 0, label: 'London (BST)' },
-		{ value: 1 * 60, label: 'Cairo (EET), Johannesburg (SAST), Paris (CEST)' },
-		{ value: 2 * 60, label: 'Jerusalem (IDT), Moscow (MSK)' },
-		{ value: 4 * 60 + 30, label: 'Chennai (IST)' },
-		{ value: 6 * 60, label: 'Hanoi (ICT), Jakarta (WIB)' },
-		{ value: 7 * 60, label: 'Hong Kong (HKT), Perth (AWST), Singapore (SGT)' },
-		{ value: 8 * 60, label: 'Tokyo (JST)' },
-		{ value: 9 * 60, label: 'Sydney (AEST)' }
+		{ value: -8 * 60, label: 'UTC-08:00 - San Francisco (PDT)' },
+		{ value: -7 * 60, label: 'UTC-07:00 - Salt Lake City (MDT)' },
+		{ value: -6 * 60, label: 'UTC-06:00 - Chicago (CDT), Mexico City (CDT)' },
+		{ value: -5 * 60, label: 'UTC-05:00 - New York City (EDT), Santiago (CLT)' },
+		{ value: -4 * 60, label: 'UTC-04:00 - Buenos Aires (ART), São Paulo (BRT)' },
+		{ value: -1 * 60, label: 'UTC-01:00 - Accra (GMT), Reykjavik (GMT)' },
+		{ value: 0, label: 'UTC+00:00 - London (BST)' },
+		{ value: 1 * 60, label: 'UTC+01:00 - Cairo (EET), Johannesburg (SAST), Paris (CEST)' },
+		{ value: 2 * 60, label: 'UTC+02:00 - Jerusalem (IDT), Moscow (MSK)' },
+		{ value: 4 * 60 + 30, label: 'UTC+04:30 - Chennai (IST)' },
+		{ value: 6 * 60, label: 'UTC+06:00 - Hanoi (ICT), Jakarta (WIB)' },
+		{ value: 7 * 60, label: 'UTC+07:00 - Hong Kong (HKT), Perth (AWST), Singapore (SGT)' },
+		{ value: 8 * 60, label: 'UTC+08:00 - Tokyo (JST)' },
+		{ value: 9 * 60, label: 'UTC+09:00 - Sydney (AEST)' }
 	];
 
 	let offset = 0;
