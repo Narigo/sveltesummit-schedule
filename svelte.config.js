b35a7b9cde0478a70fd4d5c651b3234e9072c8ae
Joern Bernhardt <joern.bernhardt@compose.us>
2021-08-29 00:08:48 +0200

    Format root as well
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- svelte.config.js
+++ svelte.config.js
@@ -1,45 +1,45 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
 
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
+    adapter: adapter({
+      // default options are shown
+      pages: "build",
+      assets: "build",
+      fallback: null,
+    }),
 
-		files: {
-			assets: 'static'
-		},
+    files: {
+      assets: "static",
+    },
 
-		paths: {
-			// base: '/sveltesummit-schedule'
-		},
+    paths: {
+      // base: '/sveltesummit-schedule'
+    },
 
-		vite: {
-			css: {
-				modules: {
-					localsConvention: 'dashesOnly',
-					scopeBehaviour: 'local'
-				}
-			},
-			server: {
-				hmr: {
-					port: 3001
-				}
-			}
-		}
-	}
+    vite: {
+      css: {
+        modules: {
+          localsConvention: "dashesOnly",
+          scopeBehaviour: "local",
+        },
+      },
+      server: {
+        hmr: {
+          port: 3001,
+        },
+      },
+    },
+  },
 };
 
 export default config;
