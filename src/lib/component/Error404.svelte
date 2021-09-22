2b7de3b6031d2feccdb0834675655173266cea41
Joern Bernhardt <joern.bernhardt@compose.us>
2021-09-23 00:47:37 +0200

    Add 404 error
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

---
+++ src/lib/component/Error404.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let error: any;
+</script>
+
+<h1>404 - Not found</h1>
+<p>The page could not be found.</p>
+<pre>{JSON.stringify(error)}</pre>
--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -2,14 +2,24 @@
   export function load({ error, status }) {
     return {
       props: {
+        error,
+        statusCode: status,
         title: `${status}: ${error.message}`,
       },
     };
   }
 </script>
 
-<script>
-  export let title;
+<script lang="ts">
+  import Error404 from "$lib/component/Error404.svelte";
+
+  export let error: any;
+  export let statusCode: number;
+  export let title: string;
 </script>
 
-<h1>{title}</h1>
+{#if statusCode === 404}
+  <Error404 {error} />
+{:else}
+  <h1>{title}</h1>
+{/if}
