14db0a0c31daf56f952521ea01f2bcc9aaecf337
Joern Bernhardt <joern.bernhardt@compose.us>
2021-10-12 23:59:16 +0200

    Add variants for later styling to schedules
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/service/schedules.ts
+++ src/lib/service/schedules.ts
@@ -1,12 +1,19 @@
-export type ScheduleMeta = { value: string; label: string; start: Date };
+export type ScheduleMeta = {
+  variant: "spring" | "fall";
+  value: string;
+  label: string;
+  start: Date;
+};
 
 const availableSchedules: ScheduleMeta[] = [
   {
+    variant: "spring",
     value: `svelte-summit-2021-spring`,
     label: "Svelte Summit Spring 2021",
     start: new Date("2021-04-25T14:00:00Z"),
   },
   {
+    variant: "fall",
     value: `svelte-summit-2021-fall`,
     label: "Svelte Summit Fall 2021",
     start: new Date("2021-11-20T14:00:00Z"),
