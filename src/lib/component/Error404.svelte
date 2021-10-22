bac8d85cda27a8bfdce49a9e3a17eea2f6522579
Joern Bernhardt <joern.bernhardt@compose.us>
2021-10-22 23:06:40 +0200

    Use layout for 404 page
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/lib/component/Error404.svelte
+++ src/lib/component/Error404.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
+  import Layout from "./layout.svelte";
+  import Link from "./link.svelte";
+
   export let path: string;
 </script>
 
-<h1>404 - Not found</h1>
-<p>The page {path} could not be found.</p>
+<Layout>
+  <h1 slot="header">404 - Not found</h1>
+  <p>The page {path} could not be found.</p>
+  <Link to="/">Main page</Link>
+  <footer slot="footer" />
+</Layout>
